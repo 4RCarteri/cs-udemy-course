2850560a0507be5bf56c4c6f1b0e33128d072727
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-01-23 16:37:29 -0300

    Add MyConsoleApp project to solution
    A new project named "MyConsoleApp" has been added to the solution file `First.sln`. This project is defined with the unique identifier `{4FBF9ED1-F89A-4A83-80A7-0BEB59233FD3}` and is located at `MyConsoleApp\MyConsoleApp.csproj`.
    
    The solution configuration platforms section in `First.sln` has been updated to include build configurations for the new "MyConsoleApp" project. Specifically, configurations for Debug and Release modes for Any CPU have been added.
    
    A new project file `MyConsoleApp.csproj` has been created with the following properties:
    - It uses the `Microsoft.NET.Sdk`.
    - The output type is set to `Exe`.
    - The target framework is set to `net8.0`.
    - Implicit usings and nullable reference types are enabled.
    
    A new `Program.cs` file has been added to the "MyConsoleApp" project. This file defines a namespace `MyConsoleApp` and contains a `Program` class with a `Main` method. The `Main` method includes two regions:
    - The first region writes "Hello, World!" to the console four times.
    - The second region writes "Other commands:" to the console five times.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region write first lines
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            #endregion
+
+            #region write other commands
+            Console.WriteLine("Other commands:");
+            Console.WriteLine("Other commands:");
+            Console.WriteLine("Other commands:");
+            Console.WriteLine("Other commands:");
+            Console.WriteLine("Other commands:");
+            #endregion
+        }
+    }
+}
