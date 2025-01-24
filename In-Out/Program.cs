3ae3bcb9f2c7a817e9bc744724330fa22772f7fe
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-01-24 11:53:13 -0300

    Add new "In-Out" project to solution
    A new project named "In-Out" has been added to the Visual Studio solution file `First.sln`. This includes the project configuration and build settings for both Debug and Release configurations.
    
    - Added `App.config` to specify .NET Framework version 4.8.
    - Created `In-Out.csproj` defining project properties and references.
    - Added `Program.cs` with a simple console application.
    - Included `AssemblyInfo.cs` with assembly metadata.

---
+++ In-Out/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Breaking line");
+            Console.Write("Without breaking line");
+
+            // caching ascii code
+            int code = Console.Read();
+            Console.WriteLine(code);
+
+            // reading a line
+            string line = Console.ReadLine();
+            Console.WriteLine(line);
+            Console.ReadKey();
+        }
+    }
+}
