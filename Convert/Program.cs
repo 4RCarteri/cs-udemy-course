545ca5d62e819afb53852ef8c8b7c17cc51c4cd8
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-01-28 09:53:24 -0300

    Add new "Convert" project with type conversion examples
    A new project named "Convert" has been added to the solution:
    - Updated `First.sln` with project GUID `{605045DC-7CA0-4279-B6DF-797DD0ED3C80}`.
    - Added build configurations for Debug and Release modes.
    - Included `App.config` targeting .NET Framework 4.8.
    - Created `Convert.csproj` defining project structure and references.
    - Added `Program.cs` with C# type conversion examples.
    - Added `AssemblyInfo.cs` with assembly metadata.

---
+++ Convert/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Implicit convertion
+            byte b = 100; // 8 bits, 0 to 255
+            ushort us; // 16 bits, 0 to 65.535
+
+            us = b;
+
+            // error
+            // us = 1000; 
+            // b = us;
+            #endregion
+
+            #region Explicit convert
+            // It can be done by casting but it can cause data loss
+            ushort num1 = 1000;
+            byte num2= (byte) num1;
+            Console.WriteLine(num2); // 232
+            #endregion
+        }
+    }
+}
---
+++ Convert/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Convert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wex")]
+[assembly: AssemblyProduct("Convert")]
+[assembly: AssemblyCopyright("Copyright © Wex 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("605045dc-7ca0-4279-b6df-797dd0ed3c80")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
