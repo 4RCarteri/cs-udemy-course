b4cb22ab9b5c145d70e4bf8f7562ba6d32111dc5
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-05 09:40:18 -0300

    Add new RegistrationSystem project with initial setup
    A new project named "RegistrationSystem" has been added to the solution. This includes the addition of the project to the solution file (.sln), and the creation of several new files for the project.
    
    - Added `App.config` specifying .NET Framework 4.8.
    - Created `Form1` with various UI elements and its associated files (`Form1.cs`, `Form1.Designer.cs`, `Form1.resx`).
    - Added `Program.cs` containing the main entry point for the application.
    - Created `RegistrationSystem.csproj` defining project properties, references, and included files.
    - Added `AssemblyInfo.cs` with assembly metadata.
    - Introduced auto-generated code for resource management and application settings:
      - `Resources.Designer.cs` for strongly-typed access to resources.
      - `Resources.resx` for storing resources in XML format.
      - `Settings.Designer.cs` for strongly-typed access to application settings.
      - `Settings.settings` for defining application settings structure.

---
+++ RegistrationSystem/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace RegistrationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.DateTimePicker();
+            this.comboMS = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.checkHouse = new System.Windows.Forms.CheckBox();
+            this.checkVehicle = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioM = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.radioO = new System.Windows.Forms.RadioButton();
+            this.list = new System.Windows.Forms.ListBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(12, 27);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(68, 25);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name";
+            this.Name.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date of birth";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marital status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(178, 21);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(354, 31);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(178, 74);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(354, 31);
+            this.txtData.TabIndex = 5;
+            // 
+            // comboMS
+            // 
+            this.comboMS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboMS.FormattingEnabled = true;
+            this.comboMS.Location = new System.Drawing.Point(178, 127);
+            this.comboMS.Name = "comboMS";
+            this.comboMS.Size = new System.Drawing.Size(354, 33);
+            this.comboMS.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(178, 182);
+            this.txtPhone.Mask = "(00) 00000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(354, 31);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // checkHouse
+            // 
+            this.checkHouse.AutoSize = true;
+            this.checkHouse.Location = new System.Drawing.Point(178, 235);
+            this.checkHouse.Name = "checkHouse";
+            this.checkHouse.Size = new System.Drawing.Size(77, 17);
+            this.checkHouse.TabIndex = 8;
+            this.checkHouse.Text = "Has house";
+            this.checkHouse.UseVisualStyleBackColor = true;
+            // 
+            // checkVehicle
+            // 
+            this.checkVehicle.AutoSize = true;
+            this.checkVehicle.Location = new System.Drawing.Point(178, 274);
+            this.checkVehicle.Name = "checkVehicle";
+            this.checkVehicle.Size = new System.Drawing.Size(130, 17);
+            this.checkVehicle.TabIndex = 9;
+            this.checkVehicle.Text = "Do you have vehicle?";
+            this.checkVehicle.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioO);
+            this.groupBox1.Controls.Add(this.radioF);
+            this.groupBox1.Controls.Add(this.radioM);
+            this.groupBox1.Location = new System.Drawing.Point(178, 313);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sex";
+            // 
+            // radioM
+            // 
+            this.radioM.AutoSize = true;
+            this.radioM.Location = new System.Drawing.Point(7, 20);
+            this.radioM.Name = "radioM";
+            this.radioM.Size = new System.Drawing.Size(48, 17);
+            this.radioM.TabIndex = 0;
+            this.radioM.TabStop = true;
+            this.radioM.Text = "Male";
+            this.radioM.UseVisualStyleBackColor = true;
+            // 
+            // radioF
+            // 
+            this.radioF.AutoSize = true;
+            this.radioF.Location = new System.Drawing.Point(6, 43);
+            this.radioF.Name = "radioF";
+            this.radioF.Size = new System.Drawing.Size(59, 17);
+            this.radioF.TabIndex = 1;
+            this.radioF.TabStop = true;
+            this.radioF.Text = "Female";
+            this.radioF.UseVisualStyleBackColor = true;
+            // 
+            // radioO
+            // 
+            this.radioO.AutoSize = true;
+            this.radioO.Location = new System.Drawing.Point(7, 66);
+            this.radioO.Name = "radioO";
+            this.radioO.Size = new System.Drawing.Size(51, 17);
+            this.radioO.TabIndex = 2;
+            this.radioO.TabStop = true;
+            this.radioO.Text = "Other";
+            this.radioO.UseVisualStyleBackColor = true;
+            // 
+            // list
+            // 
+            this.list.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 25;
+            this.list.Location = new System.Drawing.Point(17, 506);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(1001, 204);
+            this.list.TabIndex = 11;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(17, 454);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(136, 46);
+            this.btnRegister.TabIndex = 12;
+            this.btnRegister.Text = "Register / Alter";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(449, 454);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 46);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(881, 454);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(136, 46);
+            this.btnClean.TabIndex = 14;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 742);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkVehicle);
+            this.Controls.Add(this.checkHouse);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.comboMS);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Name);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker txtData;
+        private System.Windows.Forms.ComboBox comboMS;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.CheckBox checkHouse;
+        private System.Windows.Forms.CheckBox checkVehicle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.RadioButton radioM;
+        private System.Windows.Forms.RadioButton radioO;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
