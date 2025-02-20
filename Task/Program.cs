4eba3179441c09ec4889afdcdc68e32e9a0f8e29
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-20 13:42:09 -0300

    Add new Tasks project with configuration and main logic
    - Updated solution file to include "Tasks" project.
    - Added App.config to specify supported runtime version.
    - Created Program.cs with task execution examples.
    - Defined project properties and references in Tasks.csproj.
    - Added AssemblyInfo.cs for assembly metadata.

---
+++ Task/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task part 1
+            //Task task = new Task(Task1);
+            //task.Start();
+
+            //Task.Run(Task1);
+
+            Task.Factory.StartNew(Task1);
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Lambda task: " + i);
+                    System.Threading.Thread.Sleep(500);
+                }
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main: " + i);
+                System.Threading.Thread.Sleep(200);
+            }
+            #endregion
+
+
+        }
+
+        static private void Task1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Task: " + i);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
