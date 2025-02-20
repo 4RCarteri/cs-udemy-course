b61e640f7178a0f7d1a666d4c5243640073fb192
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-20 11:17:23 -0300

    Add new "Threads" project with multithreading example
    This commit introduces a new project named "Threads" to the solution. It includes the project file `Threads.csproj`, an `App.config` for runtime configuration, and a `Program.cs` file demonstrating a simple multithreading example using the `Thread` class. Additionally, an `AssemblyInfo.cs` file was added to provide assembly metadata.

---
+++ Threads/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Task));
+            // IsBackground = true means that the thread will be terminated when the main thread is terminated
+            thread.IsBackground = true;
+            thread.Start();
+            // Join() method will wait for the thread to finish
+            thread.Join();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main: " + i);
+                Thread.Sleep(200);
+            }
+
+        }
+        static void Task()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Task: " + i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
