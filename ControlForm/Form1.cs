57e193532a0832ddd8c60c3dd590504e1c351af0
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-21 13:07:24 -0300

    Add label and button to Form1 with click event
    This commit introduces a `Label` that displays "Hello World!"
    and a `Button` labeled "Click Me!" in the `Form1` class of
    the Windows Forms application. The button's click event is
    handled to change the label's text to "Hello C#". Both
    controls are added to the form's controls collection.

--- ControlForm/Form1.cs
+++ ControlForm/Form1.cs
@@ -12,6 +12,9 @@ namespace ControlForm
 {
     public partial class Form1: Form
     {
+        Label label1;
+        Button btn1;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,7 @@ namespace ControlForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Label label1 = new Label();
+            label1 = new Label();
             label1.Location = new Point(100, 50);
             label1.AutoSize = false;
             label1.Size = new Size(500, 200);
@@ -28,7 +31,23 @@ namespace ControlForm
             label1.Font = new Font("Arial", 12);
             label1.Text = "Hello World!";
 
+            btn1 = new Button();
+            btn1.Location = new Point(250, 285);
+            btn1.Size = new Size(200, 60);
+            btn1.Text = "Click Me!";
+
+            btn1.Click += btn1_Click;
+
             this.Controls.Add(label1);
+            this.Controls.Add(btn1);
+
+
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello C#";
+        }
+
     }
 }
