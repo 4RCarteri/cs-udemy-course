1a1965f1f69cb1fa1b173f57a3d3deb68c79c19f
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-21 13:39:09 -0300

    Add 21-ErrorControl project and initial implementation
    - Created a new project "21-ErrorControl" in the solution.
    - Added project file with .NET structure and configurations.
    - Included App.config for runtime specifications.
    - Developed Form1 with error handling in button click event.
    - Generated Form1.Designer.cs for UI layout.
    - Added resource file Form1.resx for localization.
    - Implemented Program.cs as the application entry point.
    - Updated AssemblyInfo.cs with project metadata.
    - Created Resources.Designer.cs for resource management.
    - Added Resources.resx for application resources.
    - Included Settings.Designer.cs and Settings.settings for user settings management.

---
+++ ErrorControl/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ErrorControl
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] num = { 1, 2, 3, 4, 5 };
+
+            string res = "";
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    res += num[i] + " ";
+                }
+
+                label1.Text = res;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
