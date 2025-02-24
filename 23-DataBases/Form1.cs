f0e2c0960be08afb5d1e166b26cb9d64397d46b2
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-24 16:55:15 -0300

    Add SQLite connection functionality and UI updates
    - Implemented click event for `btnConnect` to handle SQLite database connection.
    - Updated namespaces in `Form1.cs` for SQLite operations.
    - Added `btnConnect_Click` method to manage database creation and connection status.
    - Modified `Form1.resx` to include metadata for user-added columns: `id`, `name`, and `email`.

--- 23-DataBases/Form1.cs
+++ 23-DataBases/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,35 @@ namespace _23_DataBases
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            string dataBase = @"db\DBSQLite.db";
+            string strConn = @"Data Source = " + dataBase + "; Version = 3;";
+
+
+            if(!File.Exists(dataBase))
+            {
+                SQLiteConnection.CreateFile(dataBase);
+            }
+            SQLiteConnection db = new SQLiteConnection(strConn);
+
+            try
+            {
+                db.Open();
+                lblResult.Text = "Connected";
+            }
+            catch (Exception ex) {
+                lblResult.Text = "Error " + ex;
+            }
+            finally
+            {
+                db.Close();
+            }
+
+            #endregion
         }
     }
 }
