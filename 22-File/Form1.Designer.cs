bcf0501f1149ec5850880579c6fb7c32517fb4f3
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-24 11:13:22 -0300

    Add new project "22-File" with Windows Forms app
    - Updated solution file to include "22-File" project.
    - Created project file `22-File.csproj` with configurations.
    - Added `App.config` for runtime version settings.
    - Implemented `Form1.cs` for file operations and UI.
    - Generated `Form1.Designer.cs` for form layout.
    - Added resource file `Form1.resx` for localization.
    - Created `Program.cs` as the application entry point.
    - Updated `AssemblyInfo.cs` with assembly metadata.
    - Generated resource management files `Resources.Designer.cs` and `Resources.resx`.
    - Created settings management files `Settings.Designer.cs` and `Settings.settings`.

---
+++ 22-File/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _22_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(31, 383);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "File Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(231, 69);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(81, 29);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ 22-File/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _22_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string folder = @"C:\";
+            string file = "test.txt";
+
+            if (!File.Exists(folder + file))
+            {
+                File.Create(folder + file).Close();
+                lblResult.Text = "File created";
+            }
+            else
+            {
+                lblResult.Text = "File already exists";
+            }
+
+            if (File.Exists(folder + file))
+            {
+                File.Copy(folder + file, folder + "copy.txt", true);
+                lblResult.Text = "File copied";
+            }
+            else
+            {
+                lblResult.Text = "File does not exist";
+            }
+
+            File.Move(folder + file, folder + "move.txt");
+            lblResult.Text = "File moved";
+
+            lblResult.Text = File.GetCreationTime(folder + file).ToString();
+
+            File.WriteAllText(folder + file, lblResult.Text);
+
+
+            File.Delete(folder + file);
+            File.Delete(folder + "copy.txt");
+            File.Delete(folder + "move.txt");
+            lblResult.Text = "File deleted";
+
+        }
+    }
+}
