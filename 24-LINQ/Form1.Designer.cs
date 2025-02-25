e207792dc1698384324ab426304a411039bc0752
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-25 16:29:08 -0300

    Add 24-LINQ project with UI and LINQ functionality
    - Updated solution file to include new project "24-LINQ".
    - Created project file with configurations and .NET references.
    - Added App.config for runtime version specification.
    - Implemented Form1.cs for UI with LINQ queries for data filtering.
    - Generated Form1.Designer.cs for form layout management.
    - Added resource file Form1.resx for localization.
    - Created Program.cs as the application entry point.
    - Updated AssemblyInfo.cs with assembly metadata.
    - Generated Resources.Designer.cs and Resources.resx for resource management.
    - Created Settings.Designer.cs and Settings.settings for application settings.

---
+++ 24-LINQ/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _24_LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(12, 15);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(252, 420);
+            this.listBox.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(270, 15);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(185, 55);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(270, 76);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(185, 31);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Location = new System.Drawing.Point(270, 113);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(185, 31);
+            this.btnWhere.TabIndex = 3;
+            this.btnWhere.Text = "Where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWhere;
+    }
+}
+
---
+++ 24-LINQ/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24_LINQ
+{
+    public partial class Form1: Form
+    {
+        List<string> names;
+        List<int> numbers;
+        Dictionary<string, double> products;
+        Dictionary<string, string> states;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            names = new List<string>
+            {
+                "Ali", "Veli", "Deli", "Mehmet", "Ayşe", "Fatma", "Hayriye", "Hüseyin", "Hasan", "Hüseyin"
+            };
+
+            numbers = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            products = new Dictionary<string, double>
+            {
+                {"Laptop", 5000},
+                {"Mouse", 50},
+                {"Keyboard", 100},
+                {"Monitor", 1500},
+                {"Tablet", 2000},
+                {"Phone", 3000},
+                {"Headphone", 200},
+                {"Speaker", 500},
+                {"Microphone", 300},
+                {"Webcam", 1000}
+            };
+
+            states = new Dictionary<string, string>
+            {
+                {"Rio de Janeiro", "Brazil"},
+                {"Istanbul", "Turkey"},
+                {"New York", "USA"},
+                {"London", "UK"},
+                {"Paris", "France"},
+                {"Berlin", "Germany"},
+                {"Tokyo", "Japan"},
+                {"Moscow", "Russia"},
+                {"Rome", "Italy"},
+                {"Madrid", "Spain"}
+            };
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+
+            #region numbers
+            IEnumerable<int> number = from num in numbers where num % 2 == 0 select num;
+
+            foreach (var item in number)
+            {
+                listBox.Items.Add(item);
+            }
+
+            // Common method, is worst than LINQ because it is not optimized
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    listBox.Items.Add(item);
+                }
+            }
+            #endregion
+
+            #region names
+
+            IEnumerable<string> name = from n in names where n.StartsWith(txtSearch.Text) select n;
+
+            listBox.Items.AddRange(name.ToArray());
+
+            #endregion
+        }
+
+        private void btnWhere_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            var res = from name in names where name.ToLower().Contains(txtSearch.Text.ToLower()) select name;
+
+            listBox.Items.AddRange(res.ToArray());
+        }
+    }
+}
