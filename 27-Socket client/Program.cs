2454d3bc303e501813084e3453f2361016ba5357
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-28 14:10:05 -0300

    Add socket client and server projects to solution
    This commit introduces two new projects: "27-Socket client" and "27-Socket server," both targeting .NET Framework 4.8. The client project includes functionality to connect to a server and send messages, while the server project listens for connections and receives messages. Each project contains an `App.config` file for runtime configuration and updated `AssemblyInfo.cs` files for metadata.

---
+++ 27-Socket client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _27_Socket_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+                socket.Connect(endPoint);
+
+                Console.WriteLine("Connected to the server");
+                Console.WriteLine("Enter a message to send to the server");
+                string info = "Testing";
+
+                byte[] buffer = Encoding.ASCII.GetBytes(info);
+                socket.Send(buffer, 0, info.Length, SocketFlags.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                socket.Close();
+            }
+        }
+    }
+}
---
+++ 27-Socket server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Socket_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            server.Bind(endPoint);
+
+            server.Listen(5);
+
+            Console.WriteLine("Server is listening...");
+
+            Socket listen = server.Accept();
+
+            byte[] buffer = new byte[1024];
+
+            int size = listen.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+            Array.Resize(ref buffer, size);
+
+            Console.WriteLine("Client: " + Encoding.UTF8.GetString(buffer));
+
+            listen.Close();
+
+            Console.WriteLine("Connection closed");
+        }
+    }
+}
