5bb1b1c55fbc55726843d657be8f8e54456048bd
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-28 16:25:39 -0300

    Add 27-TransferFileServer project and Windows Forms UI
    - Introduced a new project "27-TransferFileServer" with its project file.
    - Updated solution configuration to include the new project.
    - Created a Windows Forms application with a main form (`Form1`) featuring UI elements for user interaction.
    - Added `App.config` to specify the supported runtime version.
    - Implemented `FTServer` class for handling file transfer operations over a network socket.
    - Created `Program.cs` as the entry point for the application.
    - Added resource files for managing localized strings and resources.
    - Updated assembly information in `AssemblyInfo.cs`.
    - Included settings files for managing application settings.
    - Generated designer files to support the Windows Forms designer in Visual Studio.

---
+++ 27-TransferFileServer/FTServer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _27_TransferFileServer
+{
+    class FTServer
+    {
+        static IPEndPoint IPEndPoint;
+        static Socket ServerSocket;
+        public static string IPAdress = "127.0.0.1";
+        public static int HostPort = 1000;
+        public static string HostFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
+        public static ListBox ListMessage;
+
+        public static void StartServer()
+        {
+            try
+            {
+                IPEndPoint = new IPEndPoint(IPAddress.Parse(IPAdress), HostPort);
+                ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                ServerSocket.Bind(IPEndPoint);
+            }
+            catch (Exception ex)
+            {
+                // Using invoke because we are in a different thread
+                ListMessage.Invoke(new Action(() => ListMessage.Items.Add("Error: " + ex.Message)));
+                ListMessage.SetSelected(ListMessage.Items.Count - 1, true);
+                return;
+            }
+
+            try
+            {
+                ServerSocket.Listen(10);
+                ListMessage.Invoke(new Action(() =>
+                {
+                    ListMessage.Items.Add("Server started");
+                    ListMessage.Items.Add("Waiting for client...");
+                    ListMessage.SetSelected(ListMessage.Items.Count - 1, true);
+                }));
+
+                Socket clientSocket = ServerSocket.Accept();
+                clientSocket.ReceiveBufferSize = 16384;
+
+                byte[] clientData = new byte[124 * 50000];
+                int receivedBytesLen = clientSocket.Receive(clientData, clientData.Length, 0);
+                int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+
+                BinaryWriter bWrite = new BinaryWriter(File.Open(HostFolder + fileName, FileMode.Append));
+                bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+
+                while (receivedBytesLen > 0)
+                {
+                    receivedBytesLen = clientSocket.Receive(clientData, clientData.Length, 0);
+                    if (receivedBytesLen == 0)
+                    {
+                        bWrite.Close();
+                    }
+                    else
+                    {
+                        bWrite.Write(clientData, 0, receivedBytesLen);
+                    }
+                    ListMessage.Invoke(new Action(() =>
+                    {
+                        ListMessage.Items.Add("File received and saved as " + fileName);
+                        ListMessage.SetSelected(ListMessage.Items.Count - 1, true);
+                    }));
+                    bWrite.Close();
+                    clientSocket.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                ListMessage.Invoke(new Action(() =>
+                {
+                    ListMessage.Items.Add("Error to receive the file: " + ex.Message);
+                    ListMessage.SetSelected(ListMessage.Items.Count - 1, true);
+                }));
+            }
+            finally
+            {
+                ServerSocket.Close();
+                ServerSocket.Dispose();
+                StartServer();
+            }
+        }
+    }
+}
---
+++ 27-TransferFileServer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27_TransferFileServer
+{
+    public partial class Form1: Form
+    {
+        Task task;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            linkFolder.Text = FTServer.HostFolder;
+            FTServer.ListMessage = logsList;
+        }
+
+        private void connectServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FTServer.HostPort = (int)inputPort.Value;
+                FTServer.IPAdress = txtBoxIP.Text;
+                task = Task.Factory.StartNew(() => {
+                    FTServer.StartServer();
+                });
+            }
+            catch (Exception ex)
+            {
+                logsList.Invoke(new Action(() => logsList.Items.Add("Error: " + ex.Message)));
+                logsList.SetSelected(logsList.Items.Count - 1, true);
+            }
+        }
+
+        private void stopServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Restart();
+            }catch(Exception ex)
+            {
+                logsList.Invoke(new Action(() => logsList.Items.Add("Error: " + ex.Message)));
+                logsList.SetSelected(logsList.Items.Count - 1, true);
+            }
+        }
+
+        private void linkFolder_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() != DialogResult.Cancel)
+            {
+                FTServer.HostFolder = dialog.SelectedPath + @"\";
+                linkFolder.Text = dialog.SelectedPath;
+            }
+        }
+    }
+}
