570839dcfde0f53ea53c28be6709f2735e54221f
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-28 11:36:14 -0300

    Add 25-GDI+ project with main form and resources
    - Updated `First.sln` to include the new "25-GDI+" project.
    - Created `25-GDI+.csproj` with project properties and configurations.
    - Added `App.config` for runtime version specification.
    - Implemented `Form1.cs` for the main form with drawing functionality.
    - Generated `Form1.Designer.cs` for UI components.
    - Added `Form1.resx` for localized resources.
    - Created `Program.cs` as the application entry point.
    - Updated `AssemblyInfo.cs` with assembly metadata.
    - Generated `Resources.Designer.cs` for resource management.
    - Created `Resources.resx` for structured resource handling.
    - Generated `Settings.Designer.cs` and `Settings.settings` for application settings management.

---
+++ 25-GDI+/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25_GDI_
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            // Create a blank bitmap
+            Bitmap sheet = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // Create a graphics object from the bitmap
+            Graphics g = Graphics.FromImage(sheet);
+
+            g.Clear(Color.White);
+
+            pictureBox1.BackgroundImage = sheet;
+
+            // Save the image in the root directory of the project
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sheet.jpg");
+            sheet.Save(filePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
