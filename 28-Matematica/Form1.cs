7496807736a0dd48ad433e2c6f76347ef31cbc56
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-03-12 11:07:12 -0300

    Add 28-Matematica and 28-Operations projects
    - Updated solution file to include new projects: 28-Matematica and 28-Operations.
    - Created project files for 28-Matematica and 28-Operations with necessary configurations and references.
    - Implemented a Windows Forms application in 28-Matematica with a UI for performing addition and subtraction.
    - Added Mathematics class in both projects to handle arithmetic operations.
    - Generated resource and settings files for localization and application settings management.

---
+++ 28-Matematica/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _28_Operations;
+
+namespace _28_Matematica
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _28_Operations.Mathematics.Value1 = 1;
+            _28_Operations.Mathematics.Value2 = 2;
+
+            listBox1.Items.Add("Sum: " + _28_Operations.Mathematics.Sum());
+            listBox1.Items.Add("Subtraction: " + _28_Operations.Mathematics.Subtraction());
+        }
+    }
+}
---
+++ 28-Matematica/Mathematics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Matematica
+{
+    class Mathematics
+    {
+        public static double Value1;
+        public static double Value2;
+
+        public static double Sum()
+        {
+            return Value1 + Value2;
+        }
+
+        public static double Subtraction()
+        {
+            return Value1 - Value2;
+        }
+    }
+}
