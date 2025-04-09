25f774b791aa386c3d6c9c5f56f83b3af9b03c1e
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-04-09 09:45:19 -0300

    Add 31-CaptureKeys project with Windows Forms UI
    This commit introduces a new project named "31-CaptureKeys" to the solution, configured as a Windows Forms application targeting .NET Framework 4.8. The solution configuration has been updated to support Debug and Release builds for this project.
    
    Key files added include:
    - `Form1.cs` and `Form1.Designer.cs` for the main form and event handling.
    - `Program.cs` as the application entry point.
    - `App.config` for runtime configuration.
    - `AssemblyInfo.cs` for assembly metadata.
    - `Resources.resx` and `Resources.Designer.cs` for resource management.
    - `Settings.settings` and `Settings.Designer.cs` for application settings.
    
    The new form features buttons for capturing key presses and displaying results, enhancing user interaction and following standard project organization for maintainability.

---
+++ 31-CaptureKeys/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace _31_CaptureKeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnKeyX = new System.Windows.Forms.Button();
+            this.btnKeyEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(46, 25);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(143, 25);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Press a key...";
+            // 
+            // btnKeyX
+            // 
+            this.btnKeyX.Location = new System.Drawing.Point(41, 159);
+            this.btnKeyX.Name = "btnKeyX";
+            this.btnKeyX.Size = new System.Drawing.Size(75, 23);
+            this.btnKeyX.TabIndex = 1;
+            this.btnKeyX.Text = "Key X";
+            this.btnKeyX.UseVisualStyleBackColor = true;
+            this.btnKeyX.Click += new System.EventHandler(this.btnKeyX_Click);
+            this.btnKeyX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            // 
+            // btnKeyEnter
+            // 
+            this.btnKeyEnter.Location = new System.Drawing.Point(177, 159);
+            this.btnKeyEnter.Name = "btnKeyEnter";
+            this.btnKeyEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnKeyEnter.TabIndex = 2;
+            this.btnKeyEnter.Text = "Key Enter";
+            this.btnKeyEnter.UseVisualStyleBackColor = true;
+            this.btnKeyEnter.Click += new System.EventHandler(this.btnKeyEnter_Click);
+            this.btnKeyEnter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 242);
+            this.Controls.Add(this.btnKeyEnter);
+            this.Controls.Add(this.btnKeyX);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnKeyX;
+        private System.Windows.Forms.Button btnKeyEnter;
+    }
+}
+
---
+++ 31-CaptureKeys/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _31_CaptureKeys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Select();
+        }
+
+        private void btnKeyX_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Key X clicked";
+        }
+
+        private void btnKeyEnter_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Key enter clicked";
+        }
+
+        private void form_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)97)
+            {
+                btnKeyEnter.PerformClick();
+                lblResult.Text += " - Key a pressed";
+            }
+        }
+    }
+}
