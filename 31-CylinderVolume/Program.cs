cf000665b75aebebd6d3631d1afb5a85b431e4bb
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-05-08 10:05:27 -0300

    Add 31-CylinderVolume project to the solution
    This commit introduces a new project named "31-CylinderVolume" that includes the project file (`31-CylinderVolume.csproj`), configuration settings, and essential files such as `Program.cs`, `App.config`, and `AssemblyInfo.cs`. The project is designed to calculate the volume of a cylinder based on user input for the radius and height.

---
+++ 31-CylinderVolume/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_CylinderVolume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, h, V;
+            R = 0;
+            while (R <= 0)
+            {
+                Console.Write("Type the the cylinder base radius: ");
+                R = double.Parse(Console.ReadLine());
+            }
+
+            h = 0;
+            while (h <= 0)
+            {
+                Console.Write("Type the cylinder height: ");
+                h = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            //Base area
+            A = Math.PI * Math.Pow(R, 2);
+
+            //Volume
+            V = A * h;
+            Console.WriteLine($"Cylinder volume {V}");
+        }
+    }
+}
