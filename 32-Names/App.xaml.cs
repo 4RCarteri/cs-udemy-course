9c8226e79a22ffa1b1f579f27e3559c4f43dc185
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-05-12 15:51:01 -0300

    Add new WPF project "32-Names" to solution
    This commit introduces a new project named "32-Names" to the solution file (`First.sln`). It includes the project configuration for both Debug and Release modes, and a project file (`32-Names.csproj`) targeting .NET 8.0 for Windows.
    
    Key additions:
    - `App.xaml` and `App.xaml.cs` for application definition and resources.
    - `AssemblyInfo.cs` for WPF theme information.
    - `MainWindow.xaml` and `MainWindow.xaml.cs` for the main window layout and logic.

---
+++ 32-Names/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _32_Names
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
